a1d41aaa8baa3c2d104cce99a3e10a15713f189a
gasbaoui mohammed al amine <38020387+GasbaouiMohammedAlAmin@users.noreply.github.com>
2022-01-24 18:20:05 +0100

    Update Helper.java

--- src/main/service/Helper.java
+++ src/main/service/Helper.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dz.hashcode2022.one_pizza.onepizzahashcode2022.service;
 
 import dz.hashcode2022.one_pizza.onepizzahashcode2022.model.Customer;
