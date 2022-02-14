aed762e02ae36dfa446fa1f1c2a7d2bb214c1459
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-02-14 14:12:17 +0530

    1. Added Approach 3 and 4          2. Change Some simulator code

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,39 +7,54 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-/**
- *
- * @author amine gasa
- */
+//a_an_example -> Approach 3 -> gives score 5
+//b_basic -> Approach 3 -> gives score 5
+//c_coarse -> Approach 3 -> gives score 5
+//d_difficult -> Approach 3 -> gives score 1706
+//e_elaborate -> Approach 4 -> gives score 1471
+
 public class Main {
 
     public static void main(String args[]) throws IOException {
         Helper helper = new Helper();
-        String firstGroup[] = {"a_an_example.in.txt", "b_basic.in.txt"
-        };
-         
+        String firstGroup[] = { "a_an_example.in.txt", "b_basic.in.txt" };
+
         for (String input : firstGroup) {
-           List<Customer> customers= helper.getInputFromFile(input);
-           Map<String,Integer>allIngredient=helper.getAllIngredient();
-           Map<String,Integer> choosedIngredient=helper.algorithm_1(customers,allIngredient);
-           List<String>choosedIngredientList=helper.toArrayList(choosedIngredient);
-           helper.writeIntoFile(choosedIngredientList, input);
-           LocalScoreBoardSimulator localScoreBoardSimulator=
-                   new LocalScoreBoardSimulator(customers,choosedIngredient);
-            System.out.println("Score : "+localScoreBoardSimulator.getScore());
+            //getting input from file
+            List<Customer> customers = helper.getInputFromFile(input);
+
+            Map<String, Integer> allIngredient = helper.getAllIngredient();
+            Map<String, Integer> choosedIngredient = helper.algorithm_1(customers, allIngredient);
+
+            List<String> choosedIngredientList = helper.toArrayList(choosedIngredient);
+
+            helper.writeIntoFile(choosedIngredientList, input);
+            LocalScoreBoardSimulator localScoreBoardSimulator = new LocalScoreBoardSimulator(customers, choosedIngredient);
+
+            System.out.println("Score of" + input +" algo1 : " + localScoreBoardSimulator.getScore());
+
+            Map<String, Integer> map = helper.algorithm_3(customers);
+
+            LocalScoreBoardSimulator newsimulator = new LocalScoreBoardSimulator(customers, map);
+            System.out.println("Score of" + input +" algo3 : " + newsimulator.getScore());
+
         }
-        String secondGroup[] = {"c_coarse.in.txt","d_difficult.in.txt","e_elaborate.in.txt"
 
-        };
-        for (String input : secondGroup) {//3 1441 1658
-           List<Customer> customers= helper.getInputFromFile(input);
-           Map<String,Integer> choosedIngredient= helper.algorithm_2(customers);
-           List<String>choosedIngredientList=helper.toArrayList(choosedIngredient);
-           helper.writeIntoFile(choosedIngredientList, input);
-           LocalScoreBoardSimulator localScoreBoardSimulator=
-                   new LocalScoreBoardSimulator(customers,choosedIngredient);
-            System.out.println("Score: "+localScoreBoardSimulator.getScore());
+        String secondGroup[] = { "c_coarse.in.txt", "d_difficult.in.txt", "e_elaborate.in.txt" };
 
+        for (String input : secondGroup) {// 3 1441 1658
+            List<Customer> customers = helper.getInputFromFile(input);
+            Map<String, Integer> choosedIngredient = helper.algorithm_2(customers);
+            List<String> choosedIngredientList = helper.toArrayList(choosedIngredient);
+            helper.writeIntoFile(choosedIngredientList, input);
+            LocalScoreBoardSimulator localScoreBoardSimulator = new LocalScoreBoardSimulator(customers,
+                    choosedIngredient);
+            System.out.println("Score of" + input + " algo2 : " + localScoreBoardSimulator.getScore());
+
+            Map<String, Integer> map = helper.algorithm_3(customers);
+            LocalScoreBoardSimulator newsimulator = new LocalScoreBoardSimulator(customers, map);
+            System.out.println("Score of" + input + " algo3 : " + newsimulator.getScore());
+
+        }
     }
-}
 }
--- src/main/service/Helper.java
+++ src/main/service/Helper.java
@@ -18,18 +18,17 @@ import java.util.Map;
  * @author amine gasa
  */
 public class Helper {
-private Map<String,Integer> allIngredient;
+    private Map<String, Integer> allIngredient;
 
-    
 
     public List<Customer> getInputFromFile(String fileName) throws FileNotFoundException, IOException {
-        allIngredient=new HashMap<>();
+        allIngredient = new HashMap<>();
         List<Customer> customers = new ArrayList<Customer>();
-               
+        int nbrCustomer;
         try {
-            BufferedReader fr = new BufferedReader(new FileReader("input\\" + fileName ));
+            BufferedReader fr = new BufferedReader(new FileReader("input\\" + fileName));
             String line;
-            int nbrCustomer;
+
             nbrCustomer = Integer.parseInt(fr.readLine());// this info does not important because i used dynamic table
 
             while ((line = fr.readLine()) != null) {
@@ -38,16 +37,16 @@ private Map<String,Integer> allIngredient;
                 List<String> likedIngredientCustomer = new ArrayList();
 
                 for (int i = 1; i < likedIngredientInput.length; i++) {
-                    //i started from 1 to avoid reading the lenght ingredient (dynamic table) 
+                    // i started from 1 to avoid reading the length ingredient (dynamic table)
                     likedIngredientCustomer.add(likedIngredientInput[i]);
                     allIngredient.put(likedIngredientInput[i], 0);
                 }
-                line =fr.readLine();
+                line = fr.readLine();
                 String dislikedIngredientInput[] = line.split(" ");
                 List<String> dislikedIngredientCustomer = new ArrayList();
 
                 for (int i = 1; i < dislikedIngredientInput.length; i++) {
-                    //i started from 1 to avoid reading the lenght ingredient (dynamic table) 
+                    // i started from 1 to avoid reading the length ingredient (dynamic table)
                     dislikedIngredientCustomer.add(dislikedIngredientInput[i]);
                     allIngredient.put(dislikedIngredientInput[i], 0);
                 }
@@ -56,63 +55,172 @@ private Map<String,Integer> allIngredient;
 
             }
             fr.close();
-            
+
         } catch (IOException ex) {
-            System.out.println("ex "+ex.getMessage());
+            System.out.println("ex " + ex.getMessage());
 
         }
         return customers;
     }
+
     public Map<String, Integer> getAllIngredient() {
         return allIngredient;
     }
-   public Map<String,Integer> algorithm_1( List<Customer> customers ,Map<String,Integer>allIngredient){
-        for(Customer customer:customers){
+
+    //removes ingredient that is disliked by any customer
+    public Map<String, Integer> algorithm_1(List<Customer> customers, Map<String, Integer> allIngredient) {
+        for (Customer customer : customers) {
+            //iterating through customers dislikes
             for (String dislikedIngredient : customer.getDislikedIngredients()) {
-                if(allIngredient.containsKey(dislikedIngredient))
+                //removing ingredient that is disliked by any customer
+                if (allIngredient.containsKey(dislikedIngredient))
                     allIngredient.remove(dislikedIngredient);
             }
         }
+
         return allIngredient;
-        
+
     }
-     public List<String>toArrayList(Map<String,Integer>allIngredient){
-        List<String>choosedIngredient=new ArrayList();
+
+    public List<String> toArrayList(Map<String, Integer> allIngredient) {
+        List<String> choosedIngredient = new ArrayList();
         for (String key : allIngredient.keySet()) {
-       choosedIngredient.add(key);
-               
+            choosedIngredient.add(key);
+
+        }
+        return choosedIngredient;
     }
-    return choosedIngredient;}
-    
-   public Map<String,Integer>algorithm_2(List<Customer> customers ){
+
+  
+    public Map<String, Integer> algorithm_2(List<Customer> customers) {
         Collections.sort(customers, new CustomerComparator());
-        Map<String,Integer> choosedIngredient=new HashMap();
+        Map<String, Integer> choosedIngredient = new HashMap();
+
         for (Customer customer : customers) {
-            boolean isTakenIngredient=true;
+            boolean isTakenIngredient = true;
+            // iterate over whole dislikes ingredient;
             for (String dislikedIngredient : customer.getDislikedIngredients()) {
-                if(choosedIngredient.containsKey(dislikedIngredient)){
-                    isTakenIngredient=false;
+                if (choosedIngredient.containsKey(dislikedIngredient)) {
+                    isTakenIngredient = false;
                     break;
                 }
             }
-            if(isTakenIngredient){
-               for (String likedIngredient : customer.getLikedIngredients()) {
-                   choosedIngredient.put(likedIngredient, 0);
-               }
+
+            if (isTakenIngredient) {
+                for (String likedIngredient : customer.getLikedIngredients()) {
+                    choosedIngredient.put(likedIngredient, 0);
+                }
             }
-            
+
         }
-    return choosedIngredient;}
-    
-     public void writeIntoFile(List<String>choosedIngredient,String fileName) {
+        return choosedIngredient;
+    }
+
+    //selecting ingredients which are are more liked and less disliked
+    public Map<String, Integer> algorithm_3(List<Customer> customers) {
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map1 = new HashMap<String, Integer>();
+
+        for (Customer customer : customers) {
+
+            for (String liked : customer.getLikedIngredients()) {
+                if (map.containsKey(liked)) {
+                    map.put(liked, map.get(liked) + 1);
+                } else {
+                    map.put(liked, 1);
+                }
+            }
+
+            for (String disliked : customer.getDislikedIngredients()) {
+                if (map.containsKey(disliked)) {
+                    map.put(disliked, map.get(disliked) - 1);
+                } else {
+                    map.put(disliked, 0);
+                }
+            }
+        }
+
+        // preparing string of final ingredients
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+
+            if (entry.getValue() > 0) {
+                map1.put(entry.getKey(), entry.getValue());
+            }
+
+        }
+
+        return map1;
+
+    }
+
+    public List<String> toArrayListAlgo3(Map<String, Integer> allIngredient) {
+        List<String> choosedIngredient = new ArrayList();
+
+        // preparing string of final ingredients
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+
+            if (entry.getValue() > 0) {
+                choosedIngredient.add(entry.getKey());
+            }
+
+        }
+
+        return choosedIngredient;
+    }
+
+    //Approach
+    //1. Remove Customer which dislikes are move than 2
+    //2. Selecting Ingredients whose likes are more than dislikes
+    public Map<String, Integer> algorithm_4(List<Customer> customers){
+        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map1 = new HashMap<String, Integer>();
+
+        for (Customer customer : customers) {
+
+            //skipping customer whose disliked are more
+            if(customer.getDislikedIngredients().size() > 2) continue;
+
+            for (String liked : customer.getLikedIngredients()) {
+                if (map.containsKey(liked)) {
+                    map.put(liked, map.get(liked) + 1);
+                } else {
+                    map.put(liked, 1);
+                }
+            }
+
+            for (String disliked : customer.getDislikedIngredients()) {
+                if (map.containsKey(disliked)) {
+                    map.put(disliked, map.get(disliked) - 1);
+                } else {
+                    map.put(disliked, 0);
+                }
+            }
+        }
+
+        // preparing string of final ingredients
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+
+            if (entry.getValue() > 0) {
+                map1.put(entry.getKey(), entry.getValue());
+            }
+
+        }
+
+        return map1;
+    }
+
+
+    public void writeIntoFile(List<String> choosedIngredient, String fileName) {
         try {
             System.out.println("\n------- output " + fileName);
             PrintWriter outputFile = new PrintWriter("output\\" + fileName + ".out", "UTF-8");
-            outputFile.print(choosedIngredient.size()+" ");
-            for(String ingredient:choosedIngredient){
-                outputFile.print(ingredient+" ");
+            outputFile.print(choosedIngredient.size() + " ");
+            for (String ingredient : choosedIngredient) {
+                outputFile.print(ingredient + " ");
             }
-            
+
             outputFile.close();
         } catch (Exception e) {
             System.err.println("" + e);
